4ab55e164178262bd74f6aa41da1dd543079f332
Tertius <tertiuso@gmail.com>
2019-08-03 16:02:15 +0200

    body parser size increased

--- server/controllers/logs.ctrl.js
+++ server/controllers/logs.ctrl.js
@@ -1,14 +1,20 @@
-class logsCtrl {
-	constructor(app) { this.app = app; }
+const bodyParser = require('body-parser');
 
-	postLogs(req, res, next) {
-		// TODO
+class logsCtrl {
+    constructor(app) {
+        this.app = app;
+    }
+
+    postLogs(req, res, next) {
+        
+        console.log(JSON.stringify(req.body));
+
         if (req.body) {
             const DATA = {
                 id_log: null,
                 data: JSON.stringify(req.body)
             };
-            this.app.entities.get('logs').getQuery('create').run(DATA).then( d => res.send('Ok') ).catch(e => res.send(e));
+            this.app.entities.get('log').getQuery('create').run(DATA).then( d => res.send('Ok') ).catch(e => res.send(e));
         } else {
             res.send('Ok, No data received.')
         }
